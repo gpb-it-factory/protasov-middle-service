a9dd797f96e8588a96139723e34320b68be03cc1
Protasov Aleksandr <71088795+tymillnyc@users.noreply.github.com>
2024-05-30 23:14:26 +0300

    Создан репозиторий для middle-сервиса (#2)
    * Создан репозиторий для middle-сервиса
    
    * Изменен Readme

---
+++ src/main/java/ru/gazprombank/middle/MiddleApplication.java
@@ -0,0 +1,13 @@
+package ru.gazprombank.middle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gazprombank/middle/MiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gazprombank.middle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
